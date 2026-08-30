085ba65fdcc4bbf0c8dd12c113784bf8cab711a3
Pamarthi Lalitha Naga Mallika <pamarthipll3446@gmail.com>
2026-08-30 16:34:15 +0530

    Reorder and add tanstackStart plugin in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import { defineConfig } from 'vite';
 import viteReact from '@vitejs/plugin-react';
+import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 
 export default defineConfig({
   plugins: [
     tanstackStart(),
-    viteReact()
+    viteReact(),
   ],
 });
